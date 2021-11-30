4c913e9c9c3a1219d7b07557974496fe19916f80
MARTIN CORTEZ LOPEZ <I20050008@monclova.tecnm.mx>
2021-11-30 06:00:48 -0600

    Agregar archivos de proyecto.

---
+++ ArregloCola/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace ArregloCola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEncolar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDesencolar = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.txt0 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEncolar
+            // 
+            this.btnEncolar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEncolar.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnEncolar.Location = new System.Drawing.Point(12, 12);
+            this.btnEncolar.Name = "btnEncolar";
+            this.btnEncolar.Size = new System.Drawing.Size(109, 41);
+            this.btnEncolar.TabIndex = 2;
+            this.btnEncolar.Text = "Encolar";
+            this.btnEncolar.UseVisualStyleBackColor = true;
+            this.btnEncolar.Click += new System.EventHandler(this.btnEncolar_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(267, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(125, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(218, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Numero de elementos";
+            // 
+            // btnDesencolar
+            // 
+            this.btnDesencolar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDesencolar.Location = new System.Drawing.Point(12, 59);
+            this.btnDesencolar.Name = "btnDesencolar";
+            this.btnDesencolar.Size = new System.Drawing.Size(107, 41);
+            this.btnDesencolar.TabIndex = 7;
+            this.btnDesencolar.Text = "Desencolar";
+            this.btnDesencolar.UseVisualStyleBackColor = true;
+            this.btnDesencolar.Click += new System.EventHandler(this.btnDesencolar_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.BackColor = System.Drawing.Color.Black;
+            this.txt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt1.ForeColor = System.Drawing.Color.DarkRed;
+            this.txt1.Location = new System.Drawing.Point(127, 12);
+            this.txt1.Multiline = true;
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(60, 43);
+            this.txt1.TabIndex = 8;
+            this.txt1.Text = "00";
+            this.txt1.Visible = false;
+            // 
+            // txt4
+            // 
+            this.txt4.BackColor = System.Drawing.Color.Black;
+            this.txt4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt4.ForeColor = System.Drawing.Color.DarkRed;
+            this.txt4.Location = new System.Drawing.Point(325, 12);
+            this.txt4.Multiline = true;
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(60, 43);
+            this.txt4.TabIndex = 9;
+            this.txt4.Text = "00";
+            this.txt4.Visible = false;
+            // 
+            // txt2
+            // 
+            this.txt2.BackColor = System.Drawing.Color.Black;
+            this.txt2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt2.ForeColor = System.Drawing.Color.DarkRed;
+            this.txt2.Location = new System.Drawing.Point(193, 12);
+            this.txt2.Multiline = true;
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(60, 43);
+            this.txt2.TabIndex = 10;
+            this.txt2.Text = "00";
+            this.txt2.Visible = false;
+            // 
+            // txt3
+            // 
+            this.txt3.BackColor = System.Drawing.Color.Black;
+            this.txt3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt3.ForeColor = System.Drawing.Color.DarkRed;
+            this.txt3.Location = new System.Drawing.Point(259, 12);
+            this.txt3.Multiline = true;
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(60, 43);
+            this.txt3.TabIndex = 11;
+            this.txt3.Text = "00";
+            this.txt3.Visible = false;
+            // 
+            // txt5
+            // 
+            this.txt5.BackColor = System.Drawing.Color.Black;
+            this.txt5.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt5.ForeColor = System.Drawing.Color.DarkRed;
+            this.txt5.Location = new System.Drawing.Point(391, 12);
+            this.txt5.Multiline = true;
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(60, 43);
+            this.txt5.TabIndex = 12;
+            this.txt5.Text = "00";
+            this.txt5.Visible = false;
+            // 
+            // txt0
+            // 
+            this.txt0.BackColor = System.Drawing.Color.Black;
+            this.txt0.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt0.ForeColor = System.Drawing.Color.DarkBlue;
+            this.txt0.Location = new System.Drawing.Point(193, 93);
+            this.txt0.Multiline = true;
+            this.txt0.Name = "txt0";
+            this.txt0.Size = new System.Drawing.Size(60, 43);
+            this.txt0.TabIndex = 13;
+            this.txt0.Text = "00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(667, 178);
+            this.Controls.Add(this.txt0);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.btnDesencolar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEncolar);
+            this.ForeColor = System.Drawing.Color.DarkRed;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnEncolar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDesencolar;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt5;
+        private System.Windows.Forms.TextBox txt0;
+    }
+}
+
---
+++ ArregloCola/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArregloCola
+{
+    public partial class Form1 : Form
+    {
+        int NumDatos = 0;
+        Random objeto = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void btnEncolar_Click_1(object sender, EventArgs e)
+        {
+            if (NumDatos == 5)
+            {
+                MessageBox.Show("Cola llena");
+            }
+            else
+            {
+                NumDatos++;
+                txt0.Text = Convert.ToString(NumDatos);
+                if (NumDatos == 1)
+                {
+                    txt1.Text = Convert.ToString(objeto.Next(1, 99));
+                    txt1.Visible = true;
+                }
+                if (NumDatos == 2)
+                {
+                    txt2.Text = Convert.ToString(objeto.Next(1, 99));
+                    txt2.Visible = true;
+                }
+                if (NumDatos == 3)
+                {
+                    txt3.Text = Convert.ToString(objeto.Next(1, 99));
+                    txt3.Visible = true;
+                }
+                if (NumDatos == 4)
+                {
+                    txt4.Text = Convert.ToString(objeto.Next(1, 99));
+                    txt4.Visible = true;
+                }
+                if (NumDatos == 5)
+                {
+                    txt5.Text = Convert.ToString(objeto.Next(1, 99));
+                    txt5.Visible = true;
+                }
+            }
+        }
+
+        private void btnDesencolar_Click(object sender, EventArgs e)
+        {
+            if(NumDatos == 0)
+            {
+                MessageBox.Show("La cola esta vacia");
+            }
+            else
+            {
+                txt1.Text = txt2.Text;
+                txt2.Text = txt3.Text;
+                txt3.Text = txt4.Text;
+                txt4.Text = txt5.Text;
+                if (NumDatos == 1) txt1.Visible = false;
+                if (NumDatos == 2) txt2.Visible = false;
+                if (NumDatos == 3) txt3.Visible = false;
+                if (NumDatos == 4) txt4.Visible = false;
+                if (NumDatos == 5) txt5.Visible = false;
+                NumDatos--;
+                txt0.Text = Convert.ToString(NumDatos);
+
+            }
+        }
+    }
+}
